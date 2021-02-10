88ef4196f572da148ead5225a86f4d449068528d
Rob Kimball <kimball.r@me.com>
2021-02-09 21:16:24 -0500

    Implements BFS

--- w2_search_algos/driver.py
+++ w2_search_algos/driver.py
@@ -5,9 +5,12 @@ Submission for Project 1 of Columbia University's AI EdX course (8-puzzle).
     date: 2/9/2021
 """
 
+import os
 import sys
 import math
 import time
+import psutil
+from collections import deque
 from multiprocessing import Queue
 
 
@@ -16,8 +19,8 @@ class PuzzleState(object):
 
     def __init__(self, config, n, parent=None, action="Initial", cost=0):
 
-        if n*n != len(config) or n < 2:
-            raise Exception("the length of config is not correct!")
+        if n * n != len(config) or n < 2:
+            raise ValueError("The length of config is not correct!")
 
         self.n = n
         self.cost = cost
@@ -109,29 +112,85 @@ class PuzzleState(object):
         return self.children
 
 
-def write_output():
+PROCESS = psutil.Process(os.getpid())
+MEM_USAGE = {}
+
+
+def record_usage():
+    """
+    Create a timestamped snapshot of the current process' RAM usage.
+
+    :return: None, result appended to MEM_USAGE global
+    """
+    global MEM_USAGE
+    MEM_USAGE[time.time()] = PROCESS.memory_info().rss
+
+
+def write_output(
+        algo: str,
+        state: PuzzleState,
+        max_depth: int,
+        expanded: int,
+):
     """
     Function that writes to output.txt
 
     :return:
     """
-    ### Student Code Goes here
-    pass
+    this, depth, actions = state, 1, []
+    while this.parent is not None:
+        actions.append(this.action)
+        depth += 1
+        this = this.parent
+
+    output = {
+        'path_to_goal': list(reversed(actions)),
+        'cost_of_path': len(actions),
+        'nodes_expanded': expanded,
+        'search_depth': state.cost,
+        'max_search_depth': max_depth,
+        'running_time': max(MEM_USAGE.keys()) - min(MEM_USAGE.keys()),
+        'max_ram_usage': round(max(MEM_USAGE.values()) * (1024 * 10 ** -10), 2),
+    }
+
+    fnam = f'{algo}_output.txt'
+    contents = '\n'.join([f'{k}: {v}' for k, v in output.items()])
+    with open(fnam, 'w') as fo:
+        fo.write(contents)
+    print(contents)
+
+    print(f'Output file saved to {fnam}')
 
 
-def bfs_search(initial_state):
+def bfs_search(initial_state: PuzzleState):
     """
-    BFS Search
+    Implements a breadth-first search algorithm to solve the 8-puzzle game.
 
-    :param initial_state:
-    :return:
+    :param initial_state: PuzzleState object
+    :return: N/A
     """
+    frontier, explored = deque(), set()
+    frontier.extend(initial_state.expand())
+    explored.add(initial_state.config)
+    max_depth = 0
 
-    ### STUDENT CODE GOES HERE ###
-    pass
+    while frontier:
+        record_usage()
+        state = frontier.popleft()
+        max_depth = max(max_depth, state.cost + 1)
+
+        if test_goal(state):
+            return write_output('bfs', state, max_depth, len(explored))
+
+        for node in state.expand():
+            if node.config not in explored:
+                explored.add(state.config)
+                frontier.append(node)
+
+    print('No solution found.')
 
 
-def dfs_search(initial_state):
+def dfs_search(initial_state: PuzzleState):
     """
     DFS search
 
@@ -143,7 +202,7 @@ def dfs_search(initial_state):
     pass
 
 
-def a_star_search(initial_state):
+def a_star_search(initial_state: PuzzleState):
     """
     A * search
 
@@ -155,7 +214,7 @@ def a_star_search(initial_state):
     pass
 
 
-def calculate_total_cost(state):
+def calculate_total_cost(state: PuzzleState):
     """
     Calculate the total estimated cost of a state
 
@@ -179,11 +238,15 @@ def calculate_manhattan_dist(idx, value, n):
     pass
 
 
-def test_goal(puzzle_state):
-    """ Test the state is the goal state or not """
+def test_goal(puzzle_state: PuzzleState):
+    """
+    Test whether a given state is the goal state.
 
-    ### STUDENT CODE GOES HERE ###
-    pass
+    :param puzzle_state: PuzzleState object containing an iterable 'config' with numeric values.
+    :return: bool, True if the state is the goal state.
+    """
+    arr = list(puzzle_state.config)
+    return all(a < b for a, b in zip(arr, arr[1:]))
 
 
 def main():
@@ -193,33 +256,27 @@ def main():
     :return:
     """
 
+    if len(sys.argv) < 2:
+        raise ValueError('Invalid command arguments given, first should be an algo code followed by the init state.')
+
     sm = sys.argv[1].lower()
-
     begin_state = sys.argv[2].split(",")
-
     begin_state = tuple(map(int, begin_state))
-
     size = int(math.sqrt(len(begin_state)))
-
     hard_state = PuzzleState(begin_state, size)
 
-    if sm == "bfs":
+    algos = {
+        'bfs': bfs_search,
+        'dfs': dfs_search,
+        'ast': a_star_search,
+    }
 
-        bfs_search(hard_state)
-
-    elif sm == "dfs":
-
-        dfs_search(hard_state)
-
-    elif sm == "ast":
-
-        a_star_search(hard_state)
-
+    # PEP 634 is finally here! No more dictionary switch cases!
+    if sm in algos.keys():
+        algos[sm](hard_state)
     else:
-
-        print("Enter valid command arguments !")
+        raise ValueError("Enter a valid code! Algorithm should be one of {}".format(', '.join(algos.keys())))
 
 
 if __name__ == '__main__':
-
     main()
